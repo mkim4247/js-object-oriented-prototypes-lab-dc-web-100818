30eb8f835da2192f4075e119b9b9e5795aada16c
Michael Kim <m.kim4247@gmail.com>
2020-06-20 19:36:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ function BoardMember(name, homeState, training){
   this.training = training
 }
 
-BoardMember.prototype.veto = function(){
-  return 'No, I must disagree'
-}
+BoardMember.prototype.veto = function() {
+  return 'No, I must disagree';
+};
 
-BoardMember.prototype.approve = function(){
-  return 'You can do that!'
-}
+BoardMember.prototype.approve = function() {
+  return "You can do that!";
+};
 
-BoardMember.prototype.doCharity = function(){
-  return "I like to help people."
-}
+BoardMember.prototype.doCharity = function() {
+  return 'I like to help people.';
+};
 
-BoardMember.prototype.releasePressStatement = function(){
-  return "You will see great things from Scuber."
-}
+BoardMember.prototype.releasePressStatement = function() {
+  return 'You will see great things from Scuber.';
+};
 
-BoardMember.prototype.sayHi = function(){
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 }
